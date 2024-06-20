3cb43a4b5662b051290817b0c8ce36df1899577a
suresh-devsecops <suresh.devsecops@gmail.com>
2024-06-20 21:15:48 +0530

    Add files via upload

---
+++ terraform.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.46.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.16.1"
+    }
+  }
+
+  required_version = "~> 1.3"
+}
---
+++ variable.tf
@@ -0,0 +1,5 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "some-region, like us-east-1,us-east-2,us-west-1"
+}
